b40e64ff44f6f364f6d1b955847059bb6387cae0
arpitsahay18 <74142600+arpitsahay18@users.noreply.github.com>
2025-07-14 09:32:15 +0530

    Update pdfExportUtils.ts

--- src/utils/pdfExportUtils.ts
+++ src/utils/pdfExportUtils.ts
@@ -198,7 +198,7 @@ export const createPrintStyles = (fonts: Set<string> = new Set()): HTMLStyleElem
 
   // Add the font imports and additional styles to the style element
   styleElement.textContent = 
-    `${fontImports}`
+    `${fontImports}
 
     .pdf-export-container {
       height: auto !important;
@@ -299,7 +299,7 @@ export const createPrintStyles = (fonts: Set<string> = new Set()): HTMLStyleElem
         page-break-inside: avoid !important;
       }
     }
-  ;
+  `;
 
   return styleElement;
 };
